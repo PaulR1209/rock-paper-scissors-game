b68d2c0c52b7048168e2e79a309302649ef889ab
PaulR1209 Russell <paulr711@gmail.com>
2024-01-24 15:55:20 +0000

    fix errors from jshint validator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 // set value of scores
-let myScore = 0;
-let opponentScore = 0;
+var myScore = 0;
+var opponentScore = 0;
 
-document.addEventListener('DOMContentLoaded', function () {
-    // button click funtion for instructions and reset buttons
+document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("resetScore").addEventListener("click", resetScore);
-    document.getElementById("instructions").addEventListener("click", instructions);
+    var buttons = document.getElementsByClassName("button");
+    var options = ["Rock", "Paper", "Scissors"];
+
+    // button click function
+
+    document.getElementById("resetScore")
+    .addEventListener("click", resetScore);
+
+    document.getElementById("instructions")
+    .addEventListener("click", modalBox);
     modalBox();
 
-    // Button click function for game
-    let buttons = document.getElementsByClassName('button')
-    let options = ['Rock', 'Paper', 'Scissors'];
+    function buttonClickEvent(button) {
 
-    for (let button of buttons) {
-        button.addEventListener('click', function () {
-            const myInput = button.getAttribute('data-choice');
+        button.addEventListener("click", function () {
+            const myInput = button.getAttribute("data-choice");
 
             // computer input which chooses randomly
 
@@ -32,19 +36,22 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+    for (let i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        buttonClickEvent(button);
+    }
 });
 
-/** This function compares the input of the user and computer and determines result */
+/** This function compares the input of the user and computer
+ *  and determines result */
 function game(myInput, opponentInput) {
-
-    const match = `${myInput} vs ${opponentInput}`;
 
     if (myInput === opponentInput) {
 
     } else if (
-        (myInput === 'Rock' && opponentInput === 'Scissors') ||
-        (myInput === 'Paper' && opponentInput === 'Rock') ||
-        (myInput === 'Scissors' && opponentInput === 'Paper')
+        (myInput === "Rock" && opponentInput === "Scissors") ||
+        (myInput === "Paper" && opponentInput === "Rock") ||
+        (myInput === "Scissors" && opponentInput === "Paper")
     ) {
         myScore++;
     } else {
@@ -57,20 +64,20 @@ function game(myInput, opponentInput) {
  */
 function updateScore() {
 
-    document.getElementById('my-score').textContent = myScore;
-    document.getElementById('opponent-score').textContent = opponentScore;
+    document.getElementById("my-score").textContent = myScore;
+    document.getElementById("opponent-score").textContent = opponentScore;
 }
 
-/** checks if player or computer has 5 points. 
+/** checks if player or computer has 5 points.
  * If either reach 5 points, they win, if not the loop starts again */
 function checkWinner() {
     if (myScore === 5 || opponentScore === 5) {
         let winner;
 
         if (myScore === 5) {
-            winner = 'You Win!';
+            winner = "You Win!";
         } else {
-            winner = 'You Lose...';
+            winner = "You Lose...";
         }
 
         alert(winner);
@@ -86,8 +93,11 @@ function checkWinner() {
 /** changes the image based on player and computer input */
 function updateResultImage(myInput, opponentInput) {
 
-    document.getElementById('my-input-img').src = `assets/images/${myInput}.png`
-    document.getElementById('opp-input-img').src = `assets/images/${opponentInput}.png`
+    document.getElementById("my-input-img")
+        .src = `assets/images/${myInput}.png`;
+
+    document.getElementById("opp-input-img")
+        .src = `assets/images/${opponentInput}.png`;
 }
 
 /** resets the score to 0-0 upon clicking the reset button */
@@ -99,15 +109,15 @@ function resetScore() {
 
 /** opens and closes the instrcutions when clicking the instructions button */
 function modalBox() {
-    let modal = document.getElementById('my-modal');
-    let button = document.getElementById('instructions');
-    let close = document.getElementById('close');
+    let modal = document.getElementById("my-modal");
+    let button = document.getElementById("instructions");
+    let close = document.getElementById("close");
 
     button.onclick = function () {
-        modal.style.display = 'block';
-    }
+        modal.style.display = "block";
+    };
 
     close.onclick = function () {
-        modal.style.display = 'none';
-    }
+        modal.style.display = "none";
+    };
 }
